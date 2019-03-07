03b6f7b233477819bba1087fc90f37c9b9d3e986
rayvonnee@gmail.com <rayvonnee@gmail.com>
2019-03-07 15:28:04 +0530

    CHANGES TESTED

--- app/src/main/java/com/example/myapplication/PillReminderFragment.java
+++ app/src/main/java/com/example/myapplication/PillReminderFragment.java
@@ -1,10 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,16 +22,30 @@ public class PillReminderFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_pillreminder,null);
 
-        btnPillAdd = (FloatingActionButton) getActivity().findViewById(R.id.btnnewpillrem);
+        btnPillAdd = (FloatingActionButton) v.findViewById(R.id.btnnewpillrem);
+
+
+        btnPillAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.screen_area, new AddPillReminderFragment(), "fragment_screen");
+                ft.commit();
+                }
+        });
 
         return v;
 
 
+
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
 
 
 
